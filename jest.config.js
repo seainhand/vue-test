f0a0d40513ff14cdf0ed78dbdcc0192dd61c2fea
李想02 <lixiang02@baijiahulian.com>
2021-09-06 16:53:05 +0800

    feat: 终于能用了

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,15 @@
 module.exports = {
+    moduleFileExtensions: [
+        'js',
+        'ts',
+        'json',
+        // 告诉 Jest 处理 `*.vue` 文件
+        'vue'
+    ],
     preset: '@vue/cli-plugin-unit-jest',
     transform: {
-        '^.+\\.vue$': 'vue-jest'
-    }
+        '^.+\\.vue$': 'vue-jest',
+        // 用 `ts-jest` 处理 `*.ts` 文件
+        '^.+\\.tsx?$': 'ts-jest'
+    },
 };
